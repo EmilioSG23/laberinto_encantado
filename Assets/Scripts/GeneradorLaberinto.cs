58a4b40884dece795c6067a3a6ff7ae903d3d83e
EmilioSG23 <eisaenz@espol.edu.ec>
2024-08-19 15:42:21 -0500

    fix de laberinto

--- Assets/Scripts/GeneradorLaberinto.cs
+++ Assets/Scripts/GeneradorLaberinto.cs
@@ -17,6 +17,7 @@ public class GeneradorLaberinto : MonoBehaviour
             for (int y = 0; y < tamano.y; y++){
                 Vector2 posicionCelda = new Vector2 ((x -(tamano.x / 2f)) * celdaPrefab.transform.localScale.x, (y - (tamano.y / 2f)) * celdaPrefab.transform.localScale.y);
                 CeldaController celda = Instantiate(celdaPrefab, posicionCelda, Quaternion.identity, transform);
+                celda.name = $"[{x+1};{y+1}]";
                 if (!mapInstance.cells[x][y].right)
                     celda.GetComponent<CeldaController>().eliminarMuro(0);
                 if (!mapInstance.cells[x][y].left)
